f186eab8d501bcf09149d317838f4e4b981596bd
Melloman <lovelyboyy@mail.ru>
2022-10-13 22:29:26 +0300

    i

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,12 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car("BMW","Легковое","1.5т",new Driver("Азамат Муратбеков","2 года"),new Engine("170 лошадиных сил","BMW"));
         System.out.println(car.toString());
+        car.stop();
         Lorry lorry = new Lorry("Huandai","Грузовой","2.5т",new Driver("Курманбек Мамбетомуров","3года"),new Engine("250 лошадиных сил","Huandai"),"3.5тон");
         System.out.println(lorry.toString());
+        lorry.turnLeft();
         SportCar sportCar= new SportCar("Ferrari","СпортКар","1.3т",new Driver("Айдай Маматбекова","1год"),new Engine("400 лошадиных сил","Ferrari"),"300км/час");
         System.out.println(sportCar.toString());
+        sportCar.start();
     }
 }
